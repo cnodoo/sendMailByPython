6b1fd231e7aefb0c69abd1802501c77159074cdb
diaoxinqiang <qaz60437352@gmail.com>
2016-06-08 17:39:25 +0800

    x

--- sendEmail.py
+++ sendEmail.py
@@ -11,8 +11,8 @@ smtp_server = 'smtp.yeah.net'
 subject = 'title: test send email'
 msg = MIMEText('内容:发送成功 from python','plain','utf-8')#中文需参数‘utf-8'，单字节字符不需要
 msg['Subject'] = Header(subject, 'utf-8')
-msg['From'] = 'diao<qaz137@yeah.net>'
-msg['To'] = "qaz60437352@gmail.com"
+msg['From'] = 'diao<xxxx@yeah.net>'
+msg['To'] = "xxxxx@gmail.com"
 smtp = smtplib.SMTP()
 server = smtplib.SMTP(smtp_server,25) # SMTP协议默认端口是25
 server.starttls()
